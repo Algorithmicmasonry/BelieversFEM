cd8b3cc446fb29e836c263fb97c456a659d6ecf9
Algorithmicmasonry <emmanuelandalgorithms@gmail.com>
2025-07-22 00:31:52 +0100

    resolving eslint errors mehnn

--- components/auth/sign-out-button.tsx
+++ components/auth/sign-out-button.tsx
@@ -3,11 +3,10 @@
 
 import { LogOut } from "lucide-react";
 import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import React from "react";
 
 const SignOutButton = () => {
-  const router = useRouter();
+
 
   const handleClick = async () => {
     await signOut();
