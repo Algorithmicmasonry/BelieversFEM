01e0531f70417a78b0451367a08e922669e32aef
Algorithmicmasonry <emmanuelandalgorithms@gmail.com>
2025-07-22 00:16:53 +0100

    deploying afresh

--- prisma/db.ts
+++ prisma/db.ts
@@ -4,11 +4,5 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient({
-  transactionOptions: {
-    maxWait: 10000, // Wait up to 10 seconds to acquire a connection
-    timeout: 10000, // Allow 10 seconds for the transaction to complete
-  },
-});
-
+export const db = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
